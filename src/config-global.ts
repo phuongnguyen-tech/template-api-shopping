377d79fbdad4ab09c48c22571f4ee0c7df208e56
phuongnguyen-tech <hoangphuong.tech@gmail.com>
2024-07-20 21:57:15 +0700

    api demo

---
+++ src/config-global.ts
@@ -0,0 +1,5 @@
+// API
+// ----------------------------------------------------------------------
+
+export const HOST_API = 'http://localhost:7924';
+// process.env.NODE_ENV === 'production' ? process.env.PRODUCTION_API : process.env.DEV_API;
